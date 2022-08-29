6584afb39d0d6b43df3eb918df9a3ce9a211bf20
Ceciile FU <cathymarschul9317@gmail.com>
2022-08-29 18:06:24 +0200

    browseable API via generic API views

--- backend/settings.py
+++ backend/settings.py
@@ -130,3 +130,15 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# # disable the browseable API in production
+# 
+# REST_FRAMEWORK = {
+#     'DEFAULT_RENDERER_CLASSES': (
+#         'rest_framework.renderers.JSONRenderer',
+#     )
+# }
+# 
+# #
--- backend/urls.py
+++ backend/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 
 from django.views.generic import TemplateView
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html')),
+    path('', include('leads_modelDemo.urls')),
 ]
